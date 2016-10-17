eb19dfb8e3cf77422dbd63ad76fa4264d5260fa4
Keane Lumagui <keanelumagui@gmail.com>
2016-10-17 08:50:44 +0000

    redirect to http

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ var express = require("express");
 
 var app = express();
 
+app.use(function (req, res, next){
+    if(req.headers['x-forwarded-proto'] === "http"){
+        next();
+    }else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
 app.use(express.static("public"));
 
 app.listen(process.env.PORT, process.env.IP, function(){
