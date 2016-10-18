98a9daed3378f8b4ea934528feea19fc37b40236
Keane Lumagui <keanelumagui@gmail.com>
2016-10-18 04:47:12 +0000

    edit the about and example

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,10 @@ var React   = require("react");
 
 var Examples = (props) => {
    return (
-      <h3>Examples Component</h3>   
+      <div>
+         <h3>Examples Component</h3>
+         <p>This is the Examples Page</p>
+      </div>  
    );
 };
 
