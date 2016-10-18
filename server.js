434b4b11c0176ebdb0a02fdb7610ed8c09e3b66b
Keane Lumagui <keanelumagui@gmail.com>
2016-10-18 07:18:19 +0000

    update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 app.use(function (req, res, next){
-    if(req.headers['x-forwarded-proto'] === "http"){
+    if(req.headers['x-forwarded-proto'] === "https"){
         res.redirect('http://' + req.hostname + req.url);
     }else {
         next();        
